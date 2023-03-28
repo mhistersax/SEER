a8383c57dcd4e81efe4093bda93189aa978e692d
Bamigboye Oluwapelumi <bamigboyepelumi38@gmail.com>
2023-03-28 12:16:20 +0100

    SEER

---
+++ public/Script.js
@@ -0,0 +1,10 @@
+new Vue({
+    el: "#app",
+    data: {
+      devName: "Oluwapelumi",
+      devTitle: "Senior Developer",
+      devSkills: "Front-end Developer",
+      homeLink: "#"
+    },
+    methods: {},
+  });
